8baa665a1d29d442b71759311646d55480f1d8ca
D <dim@Ds-MacBook-Pro.local>
2018-03-13 22:59:37 -0400

    fix run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,7 +4,7 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "$SCRIPTPATH/../"
 #frontend init and build
 npm i
-npm run dev
+npm run build-dev
 #backend init
 cd "$SCRIPTPATH/../server"
 npm i
